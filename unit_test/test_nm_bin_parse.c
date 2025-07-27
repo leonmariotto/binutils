a13533b666668f73134af02929014aef553c63c5
Leon Mariotto <lmariott@student.42.fr>
2025-07-27 19:44:36 +0200

    Add a simple unit_test.mk along with a small test, to be improved.
    unit_test.mk should be controllable by caller script to run multiple
    tests.

---
+++ unit_test/test_nm_bin_parse.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <stdint.h>
+#include "cmocka.h"
+
+#include <elf.h>
+#include "nm_bin.h"
+
+static void test_nm_bin_parse64_ehdr(void **state)
+{
+	(void)state;
+	Elf64_Ehdr hdr = {
+		.e_ident = {0x7f, 0x45, 0x4c, 0x46, ELFCLASS64, ELFDATA2LSB, 0x11},
+		.e_phoff = 10,
+		.e_shoff = 10,
+		.e_phentsize = 10,
+		.e_phnum = 10,
+		.e_shentsize = 10,
+		.e_shnum = 10,
+	};
+	t_nm_bin bin = {
+		.mem = (void*)&hdr,
+		.length = 200,
+	};
+	int r = nm_bin_parse_ehdr(&bin);
+	assert_true(r == 0);
+	assert_true(bin.elfclass == ELFCLASS64);
+	assert_true(bin.endian == ELFDATA2LSB);
+	assert_ptr_equal(bin.ehdr64, &hdr);
+}
+
+int main(void)
+{
+    const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_nm_bin_parse64_ehdr),
+    };
+	cmocka_set_message_output(CM_OUTPUT_XML);
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
